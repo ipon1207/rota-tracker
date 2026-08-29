6335cca2f3bbbbf16c9bbecfee69d619bcdd9f9d
miyashita <ipon_1207@yahoo.co.jp>
2026-08-30 07:17:10 +0900

    chore: VSCodeのインポート補完が@/エイリアス設定を読み込むように修正

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
-import { Button } from './components/ui/button';
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0)
