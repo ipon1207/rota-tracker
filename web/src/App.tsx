e355bdc41a75b2207783b164396c43ce9f088af7
miyashita <ipon_1207@yahoo.co.jp>
2026-09-02 07:25:40 +0900

    fix: 削除したアイコン・画像を参照している部分を修正

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,7 @@ import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 
-import heroImg from './assets/hero.png';
-import reactLogo from './assets/react.svg';
-
 import './App.css';
-import viteLogo from './assets/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,11 +10,6 @@ function App() {
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
         <div>
           <h1>Get started</h1>
           <p>
@@ -40,13 +31,11 @@ function App() {
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
                 Explore Vite
               </a>
             </li>
             <li>
               <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
                 Learn more
               </a>
             </li>
